f8217c68bcc3a9a66bd71a3bb283b50a6634bb49
Øystein Hansen <oeysteinhansen@gmail.com>
2022-02-17 11:38:13 +0100

    Create ga.sh

---
+++ ga.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Ubuntu Guest Addions setup script (Ubuntu 21.10)
+# Author : Øystein Hansen
+
+sudo apt-get install -y --no-install-recommends virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
